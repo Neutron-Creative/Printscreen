f44fcd42a0f28815cff8b6ab86bbee2feb4119b7
Navid Kabir <navid113@gmail.com>
2020-08-21 15:10:59 -0400

    Add environment variables

--- src/App.ts
+++ src/App.ts
@@ -3,6 +3,14 @@ import * as configFile from "./config.json";
 
 export let config = configFile;
 
+if (process.env.PORT) {
+    config.port = Number.parseInt(process.env.PORT);
+}
+
+if (process.env.HOST) {
+    config.host = process.env.HOST;
+}
+
 console.log("Initializing Neutron Capture");
 
 let server: CaptureServer = new CaptureServer();
